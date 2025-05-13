0f14800479066fbba7c3bbaed1a22df5307ea5c7
JinOptimist <88jin@mail.ru>
2025-05-13 20:36:00 +0300

    task 3

--- js/task1.js
+++ js/task1.js
@@ -19,13 +19,43 @@ $(document).ready(function () {
         const userValue = $('.task-2 .user-input').val(); // abac
         const arrayOfSymbols = userValue.split(''); // ['a','b','a','c']
 
-        let answer = '';
+        const listWithSymbolsAndCount = [];
         for (let i = 0; i < arrayOfSymbols.length; i++) {
             const symbol = arrayOfSymbols[i]; // 'a'
-            // ???
-            // answer += ???
+
+            const goodObj = listWithSymbolsAndCount
+                .find(obj => obj.s == symbol); // {s: 'q', c: 1}
+            if (goodObj != undefined) {
+                goodObj.c++;
+            } else {
+                const newGoodObj = {
+                    s: symbol,
+                    c: 1
+                };
+                listWithSymbolsAndCount.push(newGoodObj);
+            }
+        }
+
+        let answer = '';
+        for (let index = 0; index < listWithSymbolsAndCount.length; index++) {
+            const obj = listWithSymbolsAndCount[index];
+            
+            answer += `${obj.s}: ${obj.c}; `
         }
 
         $('.task-2 .result').text(answer);
     });
+
+    let images = [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwctyTE48aLTh8txv4Tsdg53zap46VGvZJoA&s',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_ix09HQuQHM0q-OPQBUhJAW8X-mhHS9EPOQ&s'
+    ];
+
+    $('.task-3 .do').click(function () {
+        const url = images[1];
+        const img = $('<img>');
+        img.attr('src', url);
+        $('.task-3 .result').append(img);
+    });
+
 });
