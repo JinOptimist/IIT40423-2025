f838327c62fa71c50abf7aaa64114f8961961e7c
JinOptimist <88jin@mail.ru>
2025-05-20 19:53:38 +0300

    Add images to gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,22 @@
 $(document).ready(function () {
-    let isFirst = true;
+    let images = [
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwctyTE48aLTh8txv4Tsdg53zap46VGvZJoA&s',
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS_ix09HQuQHM0q-OPQBUhJAW8X-mhHS9EPOQ&s',
+        'https://m.media-amazon.com/images/I/417YwW3UwsL._AC_UF1000,1000_QL80_.jpg'
+    ];
+    init();
 
     $('.container').click(function () {
         $('.box').toggleClass('isActive');
     });
+
+    function init(){
+        images.forEach(url => {
+            const tag = $('.placeholder.template').clone();
+            tag.removeClass('template');
+            tag.find('img').attr('src', url);
+            $('.gallery').append(tag);
+        });
+    }
 
 });
