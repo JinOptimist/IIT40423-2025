1e1a366ed4290c31f686c431ad4319ef31c0d962
JinOptimist <88jin@mail.ru>
2025-06-03 20:12:20 +0300

    add images as source

--- js/carusel.js
+++ js/carusel.js
@@ -2,6 +2,11 @@ $(document).ready(function () {
     const fullSize = 300;
     const halfSize = 150;
     const animationSpeed = 2;
+    const images = [];
+    let currentImageIndex = 0;
+
+    initImages();
+
     $('.next').click(function () {
         // run animation
         setWidthAndHeight('.prev', 0);
@@ -13,6 +18,12 @@ $(document).ready(function () {
         setTimeout(setAllSizeToDefault, animationSpeed * 1000);
     })
 
+    function initImages() {
+        for (let i = 0; i < 3; i++) {
+            images.push(`images/girl-wide-0${i + 1}.jpg`);
+        }
+    }
+
     function setAllSizeToDefault() {
         $('.image-container').css('transition', 'none');
 
@@ -21,15 +32,26 @@ $(document).ready(function () {
         setWidthAndHeight('.center', fullSize);
         setWidthAndHeight('.next', halfSize);
         setWidthAndHeight('.after-next', 0);
+        
+        moveAllImages();
 
-        moveImage('.next', '.center');
-
-        setTimeout(function(){
+        setTimeout(function () {
             $('.image-container').css('transition', `all ${animationSpeed}s`);
         }, 100);
     }
 
-    function moveImage(fromSelector, toSelector){
+    function moveAllImages(){
+        moveImage('.prev', '.before-prev');
+        moveImage('.center', '.prev');
+        moveImage('.next', '.center');
+        moveImage('.after-next', '.next');
+        
+        const url = images[currentImageIndex];
+        $('.after-next').find('img').attr('src', url);
+        currentImageIndex++;
+    }
+
+    function moveImage(fromSelector, toSelector) {
         const url = $(fromSelector).find('img').attr('src');
         $(toSelector).find('img').attr('src', url);
     }
