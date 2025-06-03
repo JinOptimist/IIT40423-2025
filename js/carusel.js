43b9ab6745911686e0b367197fd274053cf83542
JinOptimist <88jin@mail.ru>
2025-06-03 19:49:59 +0300

    Add revert size to default after anuimation

--- js/carusel.js
+++ js/carusel.js
@@ -1,6 +1,34 @@
 $(document).ready(function () {
+    const fullSize = 300;
+    const halfSize = 150;
+    const animationSpeed = 2;
     $('.next').click(function () {
-        $('.prev').css('width', 0);
-        $('.after-next').css('width', 300);
+        // run animation
+        setWidthAndHeight('.prev', 0);
+        setWidthAndHeight('.center', halfSize);
+        setWidthAndHeight('.next', fullSize);
+        setWidthAndHeight('.after-next', halfSize);
+
+        // return to initial state
+        setTimeout(setAllSizeToDefault, animationSpeed * 1000);
     })
+
+    function setAllSizeToDefault() {
+        $('.image-container').css('transition', 'none');
+
+        setWidthAndHeight('.before-prev', 0);
+        setWidthAndHeight('.prev', halfSize);
+        setWidthAndHeight('.center', fullSize);
+        setWidthAndHeight('.next', halfSize);
+        setWidthAndHeight('.after-next', 0);
+
+        setTimeout(function(){
+            $('.image-container').css('transition', `all ${animationSpeed}s`);
+        }, 100);
+    }
+
+    function setWidthAndHeight(blockSelector, widthAndHeight) {
+        $(blockSelector).css('width', widthAndHeight);
+        $(blockSelector).css('height', widthAndHeight);
+    }
 })
